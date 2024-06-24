f534631e1c86feec6fb898eb89eb4aa22f142576
littl <littl@ANNA_LAPTOP>
2024-06-23 23:40:50 -0500

    Add project files.

---
+++ 2.29 - Squared and Cubed Table/2.29 - Squared and Cubed Table.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int integer = 0;
+    
+    //There's a better way of doing this with loops but with this chapter's knowledge we'll get there ;-;
+    cout << "Integer\t\t" << "Square\t\t" << "Cube\t\t" << endl;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+    cout << integer << "\t\t" << integer * integer << "\t\t" << integer * integer * integer << endl;
+    integer = integer + 1;
+}
+
